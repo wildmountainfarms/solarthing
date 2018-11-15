adfcd33c96e098015f8a470c7fb2d8f332b3e834
Joshua Shannon <retrodaredevil@gmail.com>
2018-11-15 10:11:21 -0600

    Updated jar

--- src/me/retrodaredevil/solarthing/packet/ImmutablePacketCollection.java
+++ src/me/retrodaredevil/solarthing/packet/ImmutablePacketCollection.java
@@ -30,8 +30,8 @@ class ImmutablePacketCollection implements PacketCollection {
 		this.packets = Collections.unmodifiableList(new ArrayList<>(packets));
 		final Calendar cal = new GregorianCalendar();
 		final int year = cal.get(Calendar.YEAR);
-		final int day = cal.get(Calendar.DAY_OF_MONTH);
 		final int month = cal.get(Calendar.MONTH) + 1; // [1..12]
+		final int day = cal.get(Calendar.DAY_OF_MONTH);
 		final int hour = cal.get(Calendar.HOUR_OF_DAY);
 		final int minute = cal.get(Calendar.MINUTE);
 		final int second = cal.get(Calendar.SECOND);
