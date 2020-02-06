31b84c7d51680360eb013124778e46ef8ea170c3
Joshua Shannon <retrodaredevil@gmail.com>
2020-02-06 09:53:08 -0600

    Made more event packets able to be deserialized

--- core/src/main/java/me/retrodaredevil/solarthing/solar/event/SolarEventPacket.java
+++ core/src/main/java/me/retrodaredevil/solarthing/solar/event/SolarEventPacket.java
@@ -3,14 +3,18 @@ package me.retrodaredevil.solarthing.solar.event;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import me.retrodaredevil.solarthing.packets.DocumentedPacket;
 import me.retrodaredevil.solarthing.solar.outback.fx.event.FXACModeChangePacket;
+import me.retrodaredevil.solarthing.solar.outback.fx.event.FXDayEndPacket;
 import me.retrodaredevil.solarthing.solar.outback.mx.event.MXDayEndPacket;
+import me.retrodaredevil.solarthing.solar.outback.mx.event.MXRawDayEndPacket;
 
 /**
  * A type of packet for solar data that goes into the "event" database. These packets have one of the types defined in {@link SolarEventPacketType}
  */
 @JsonSubTypes({
 		@JsonSubTypes.Type(FXACModeChangePacket.class),
-		@JsonSubTypes.Type(MXDayEndPacket.class)
+		@JsonSubTypes.Type(FXDayEndPacket.class),
+		@JsonSubTypes.Type(MXDayEndPacket.class),
+		@JsonSubTypes.Type(MXRawDayEndPacket.class),
 })
 public interface SolarEventPacket extends DocumentedPacket<SolarEventPacketType> {
 }
