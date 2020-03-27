65e07d501514c9b7a0517daf979d8a5cbe2a9f3b
Joshua Shannon <retrodaredevil@gmail.com>
2020-03-27 16:03:17 -0500

    Added test for cpu temperature packet, made CpuTemperaturePacket deserializable

---
+++ core/src/test/java/me/retrodaredevil/solarthing/misc/device/DevicePacketTest.java
@@ -0,0 +1,20 @@
+package me.retrodaredevil.solarthing.misc.device;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import me.retrodaredevil.solarthing.solar.PacketTestUtil;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DevicePacketTest {
+
+	@Test
+	void test() throws JsonProcessingException {
+		{
+			CpuTemperaturePacket packet = new CelsiusCpuTemperaturePacket(20);
+			assertEquals(20, packet.getCpuTemperatureCelsius());
+			PacketTestUtil.testJson(packet, CpuTemperaturePacket.class);
+			PacketTestUtil.testJson(packet, DevicePacket.class);
+		}
+	}
+}
--- other/linux/update_perms.sh
+++ other/linux/update_perms.sh
@@ -1,5 +1,8 @@
 echo This updates the permission of the solarthing directory. The solarthing directory must be in your current working directory...
 
-chmod 775 -R solarthing/ | exit 1
+echo This is not ready for use! It screws up some git stuff!
+exit 1
+
+chmod 770 -R solarthing/ | exit 1
 chmod g+s -R solarthing/ | exit 1
 chgrp -R solarthing solarthing/ | exit 1
