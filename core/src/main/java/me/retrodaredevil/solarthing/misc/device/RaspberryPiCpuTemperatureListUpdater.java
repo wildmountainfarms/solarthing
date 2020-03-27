81fbd0b21da5ff96868dcd3e3f21ca357eca6a70
Joshua Shannon <retrodaredevil@gmail.com>
2020-03-27 15:01:03 -0500

    Added cpu temperature packet that works on raspberry pis

---
+++ core/src/main/java/me/retrodaredevil/solarthing/misc/device/RaspberryPiCpuTemperatureListUpdater.java
@@ -0,0 +1,51 @@
+package me.retrodaredevil.solarthing.misc.device;
+
+import me.retrodaredevil.solarthing.packets.Packet;
+import me.retrodaredevil.solarthing.packets.handling.PacketListReceiver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class RaspberryPiCpuTemperatureListUpdater implements PacketListReceiver {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RaspberryPiCpuTemperatureListUpdater.class);
+	private final byte[] buffer = new byte[1024];
+	@Override
+	public void receive(List<Packet> packets, boolean wasInstant) {
+		ProcessBuilder processBuilder = new ProcessBuilder("vcgencmd", "measure_temp");
+		processBuilder.redirectErrorStream(true);
+		try {
+			Process process = processBuilder.start();
+			boolean finished = process.waitFor(100, TimeUnit.MILLISECONDS);
+			if(!finished){
+				LOGGER.warn("vcgencmd must have taken longer than 100ms!");
+			} else {
+				InputStream inputStream = process.getInputStream();
+				int len = inputStream.read(buffer);
+				String result = new String(buffer, 0, len);
+				String[] split1 = result.split("=");
+				if(split1.length != 2){
+					LOGGER.warn("split1.length != 2!!! split1.length=" + split1.length);
+					return;
+				}
+				String[] split2 = split1[1].split("'");
+				if(split2.length != 2){
+					LOGGER.warn("split2.length != 2!!! split2.length=" + split2.length);
+					return;
+				}
+				String toParse = split2[1];
+				try {
+					float resultCelsius = Float.parseFloat(toParse);
+					packets.add(new CelsiusCpuTemperaturePacket(resultCelsius));
+				} catch (NumberFormatException e){
+					LOGGER.warn("Couldn't parse! toParse=" + toParse + ". Result=" + result, e);
+				}
+			}
+		} catch (IOException | InterruptedException e) {
+			LOGGER.error("Error while running command to get CPU temperature", e);
+		}
+	}
+}
