0e7fa56551780e09126ac6294bf6db135d6e1912
Joshua Shannon <retrodaredevil@gmail.com>
2020-05-14 13:13:16 -0500

    Download script improvements, default version 2020.1.3

--- program/.scripts/export_version.sh
+++ program/.scripts/export_version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-export SOLARTHING_VERSION="2020.1.2"
+export SOLARTHING_VERSION="2020.1.3"
 
 if [ -n "${SOLARTHING_VERSION_OVERRIDE+x}" ]; then
   echo Using override
--- program/graphql_download_solarthing.sh
+++ program/graphql_download_solarthing.sh
@@ -6,9 +6,10 @@ cd "$BASEDIR" || exit 1
 # shellcheck source=.scripts/export_version.sh
 . .scripts/export_version.sh || exit 1
 
-if ! ls ".downloads/solarthing-graphql-$SOLARTHING_VERSION.jar" 1>/dev/null 2>&1; then
+if ! zip -T ".downloads/solarthing-graphql-$SOLARTHING_VERSION.jar" 1>/dev/null 2>&1; then
   echo Going to download version: "$SOLARTHING_VERSION" GraphQL
   mkdir ".downloads" 2>/dev/null
+  rm ".downloads/solarthing-graphql-$SOLARTHING_VERSION.jar" 1>/dev/null 2>&1 && echo Removed corrupt version
   (cd .downloads && wget "https://github.com/wildmountainfarms/solarthing/releases/download/v$SOLARTHING_VERSION/solarthing-graphql-$SOLARTHING_VERSION.jar") || exit 1
 else
   echo Already downloaded
