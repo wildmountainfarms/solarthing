3a74201fba999d3ef0f6e29fbfdda35e3e1f6c06
Joshua Shannon <retrodaredevil@gmail.com>
2021-02-09 22:59:46 -0600

    Fixed test. Yeah, the bug was in the test I swear

--- core/src/test/java/me/retrodaredevil/solarthing/solar/renogy/RenogyTest.java
+++ core/src/test/java/me/retrodaredevil/solarthing/solar/renogy/RenogyTest.java
@@ -25,12 +25,12 @@ final class RenogyTest {
 		assertTrue(StreetLight.OFF.isActive(~StreetLight.IGNORED_BITS & 0b01111011));
 		assertTrue(StreetLight.OFF.isActive(~StreetLight.IGNORED_BITS & 0b00111011));
 		assertTrue(StreetLight.OFF.isActive(~StreetLight.IGNORED_BITS & 0b00111010));
-		
+
 		assertTrue(StreetLight.ON.isActive(~StreetLight.IGNORED_BITS & 0b11111111));
 		assertTrue(StreetLight.ON.isActive(~StreetLight.IGNORED_BITS & 0b11111011));
 		assertTrue(StreetLight.ON.isActive(~StreetLight.IGNORED_BITS & 0b10111011));
 		assertTrue(StreetLight.ON.isActive(~StreetLight.IGNORED_BITS & 0b10111010));
-		
+
 		assertEquals(100, StreetLight.getBrightnessValue(100));
 		assertEquals(100, StreetLight.getBrightnessValue(100 | (1 << 7)));
 		assertEquals(49, StreetLight.getBrightnessValue(49));
@@ -39,32 +39,32 @@ final class RenogyTest {
 	@Test
 	void testSpecialPower_E021(){
 		MutableSpecialPowerControl_E021 power = new MutableSpecialPowerControl_E021();
-		
+
 		power.setChargingModeControlledByVoltage(true);
 		assertTrue(power.isChargingModeControlledByVoltage());
 		power.setChargingModeControlledByVoltage(false);
 		assertFalse(power.isChargingModeControlledByVoltage());
-		
+
 		power.setSpecialPowerControlEnabled(true);
 		assertTrue(power.isSpecialPowerControlEnabled());
 		power.setSpecialPowerControlEnabled(false);
 		assertFalse(power.isSpecialPowerControlEnabled());
-		
+
 		power.setEachNightOnEnabled(true);
 		assertTrue(power.isEachNightOnEnabled());
 		power.setEachNightOnEnabled(false);
 		assertFalse(power.isEachNightOnEnabled());
-		
+
 		power.setNoChargingBelow0CEnabled(true);
 		assertTrue(power.isNoChargingBelow0CEnabled());
 		power.setNoChargingBelow0CEnabled(false);
 		assertFalse(power.isNoChargingBelow0CEnabled());
-		
+
 		power.setChargingMethod(ChargingMethod.DIRECT);
 		assertEquals(ChargingMethod.DIRECT, power.getChargingMethod().getChargingMethod());
 		power.setChargingMethod(ChargingMethod.PWM);
 		assertEquals(ChargingMethod.PWM, power.getChargingMethod().getChargingMethod());
-		
+
 		assertFalse(power.isChargingModeControlledByVoltage());
 		assertFalse(power.isSpecialPowerControlEnabled());
 		assertFalse(power.isEachNightOnEnabled());
@@ -74,37 +74,37 @@ final class RenogyTest {
 	@Test
 	void testSpecialPower_E02D(){
 		MutableSpecialPowerControl_E02D power = new MutableSpecialPowerControl_E02D();
-		
+
 		power.setIntelligentPowerEnabled(true);
 		assertTrue(power.isIntelligentPowerEnabled());
 		power.setIntelligentPowerEnabled(false);
 		assertFalse(power.isIntelligentPowerEnabled());
-		
+
 		power.setEachNightOnEnabled(true);
 		assertTrue(power.isEachNightOnEnabled());
 		power.setEachNightOnEnabled(false);
 		assertFalse(power.isEachNightOnEnabled());
-		
+
 		power.setIsLithiumBattery(true);
 		assertTrue(power.isLithiumBattery());
 		power.setIsLithiumBattery(false);
 		assertFalse(power.isLithiumBattery());
-		
+
 		power.setChargingMethod(ChargingMethod.DIRECT);
 		assertEquals(ChargingMethod.DIRECT, power.getChargingMethod().getChargingMethod());
 		power.setChargingMethod(ChargingMethod.PWM);
 		assertEquals(ChargingMethod.PWM, power.getChargingMethod().getChargingMethod());
-		
+
 		power.setNoChargingBelow0CEnabled(true);
 		assertTrue(power.isNoChargingBelow0CEnabled());
 		power.setNoChargingBelow0CEnabled(false);
 		assertFalse(power.isNoChargingBelow0CEnabled());
-		
+
 		power.setIs24VSystem(true);
 		assertTrue(power.is24VSystem());
 		power.setIs24VSystem(false);
 		assertFalse(power.is24VSystem());
-		
+
 		assertFalse(power.isIntelligentPowerEnabled());
 		assertFalse(power.isEachNightOnEnabled());
 		assertFalse(power.isLithiumBattery());
@@ -117,7 +117,7 @@ final class RenogyTest {
 		Rover.OperatingSettingBundle operating = new Rover.OperatingSettingBundle(0, 0);
 		Rover.SensingBundle sensing = new Rover.SensingBundle(0, 0, 0);
 		RoverStatusPacket packet = new ImmutableRoverStatusPacket(
-			0,0,0,0,
+			12,30,0,0,
 			"     HI         ".getBytes(StandardCharsets.UTF_8),
 			0,0,
 			0,0,0,26.2f,0,
@@ -139,7 +139,7 @@ final class RenogyTest {
 
 		String json2 = mapper.writeValueAsString(parsed);
 		assertEquals(json, json2);
-		
+
 		assertFalse(parsed.isNewDay(parsed)); // because they're the same, just do a quick check to make sure this method returns false. Maybe we'll add another test for it later...
 	}
 	@Test
