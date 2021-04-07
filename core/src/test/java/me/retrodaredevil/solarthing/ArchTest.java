4c3aa98ca7f7be9b58dc84a7e91ae7a43aca6004
Joshua Shannon <retrodaredevil@gmail.com>
2021-04-07 13:26:31 -0500

    Updated some depdencies and updated ArchTest

--- core/src/test/java/me/retrodaredevil/solarthing/ArchTest.java
+++ core/src/test/java/me/retrodaredevil/solarthing/ArchTest.java
@@ -3,20 +3,32 @@ package me.retrodaredevil.solarthing;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
+import me.retrodaredevil.solarthing.annotations.UtilityClass;
 import me.retrodaredevil.solarthing.packets.DocumentedPacketType;
+import me.retrodaredevil.solarthing.packets.Mode;
 import org.junit.jupiter.api.Test;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 
 public class ArchTest {
 	@Test
 	void testArch() {
 		JavaClasses importedClasses = new ClassFileImporter().importPackages("me.retrodaredevil.solarthing");
 
-		ArchRule rule = classes().that()
-				.areNotInterfaces().and().areAssignableTo(DocumentedPacketType.class)
-				.should().beEnums();
+		for(ArchRule rule : new ArchRule[] {
+				classes().that()
+						.areNotInterfaces().and().areAssignableTo(DocumentedPacketType.class)
+						.should().beEnums(),
+				classes().that()
+						.areNotInterfaces().and().areAssignableTo(Mode.class)
+						.should().beEnums(),
+				classes().that().areEnums().should().haveOnlyFinalFields(),
 
-		rule.check(importedClasses);
+				noMethods().that().areNotStatic().should().beDeclaredInClassesThat().areAnnotatedWith(UtilityClass.class),
+				noFields().that().areNotStatic().should().beDeclaredInClassesThat().areAnnotatedWith(UtilityClass.class),
+				noConstructors().that().areNotPrivate().should().beDeclaredInClassesThat().areAnnotatedWith(UtilityClass.class),
+		}) {
+			rule.check(importedClasses);
+		}
 	}
 }
