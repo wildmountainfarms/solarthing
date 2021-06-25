b9f99c85811479145c9c65a6ca3da1560d994a4b
Joshua Shannon <retrodaredevil@gmail.com>
2021-06-24 23:25:28 -0500

    Added script to change hostname

---
+++ other/linux/set_hostname.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+
+if [ "$#" -gt 0 ]; then
+  NEW_HOSTNAME=$1
+  echo "Going set the hostname to: $NEW_HOSTNAME"
+  echo "Press enter to continue"
+  # shellcheck disable=SC2034
+  read -r UNUSED_VARIABLE || exit 1
+  hostnamectl set-hostname "$NEW_HOSTNAME" || exit 1
+  echo "127.0.0.1 $NEW_HOSTNAME" >> /etc/hosts || exit 1
+  echo "Success!"
+fi
