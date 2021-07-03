dea5c874f76a9e11ba6ae3a8bb8d2e98eb1b2011
Joshua Shannon <retrodaredevil@gmail.com>
2021-07-02 19:09:51 -0500

    Updated update.sh

--- update.sh
+++ update.sh
@@ -2,12 +2,14 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR" || exit 1
 
-git pull
+git pull || (echo "Failed to pull!"; exit 1)
+program/download_solarthing.sh || exit 1
+program/graphql_download_solarthing.sh || exit 1
+
 FULLDIR=$(pwd)
 if [ "$FULLDIR" = "/opt/solarthing" ]; then
-  other/linux/update_perms.sh continue
+  echo
+  other/linux/update_perms.sh continue || (echo "The above output may look scary, but you need to run this using sudo to update permissions"; exit 1)
 else
   echo Not updating perms because installed in non-standard location
 fi
-program/download_solarthing.sh
-program/graphql_download_solarthing.sh
