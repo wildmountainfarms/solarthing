8056a43b038284c49488ab0a853f086efa9656c5
Joshua Shannon <retrodaredevil@gmail.com>
2022-01-05 02:11:45 -0600

    Updated use_sdkman_if_available.sh to not give errors when sdkman directory not present

--- program/.scripts/use_sdkman_if_available.sh
+++ program/.scripts/use_sdkman_if_available.sh
@@ -13,8 +13,9 @@ fi
 
 
 if [ -z "${NO_SOLARTHING_SDKMAN+1}" ]; then
-  SDKMAN_DIR=$(cd "$SOLARTHING_ROOT/program/.downloads/sdkman" && pwd)
+  SDKMAN_DIR="$SOLARTHING_ROOT/program/.downloads/sdkman"
   if [ -f "$SDKMAN_DIR/bin/sdkman-init.sh" ]; then
+    SDKMAN_DIR=$(cd "$SDKMAN_DIR" && pwd)
     echo "Using sdkman installation with SDKMAN_DIR=$SDKMAN_DIR"
     export SDKMAN_DIR
     source "$SDKMAN_DIR/bin/sdkman-init.sh"
