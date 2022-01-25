c74d9f1f2db4842ad43de33fdf5c531d999c95eb
Joshua Shannon <retrodaredevil@gmail.com>
2022-01-24 19:52:39 -0600

    Fix for a test to maybe make it work in github

--- common/src/test/java/me/retrodaredevil/solarthing/actions/command/SendPacketActionTest.java
+++ common/src/test/java/me/retrodaredevil/solarthing/actions/command/SendPacketActionTest.java
@@ -23,6 +23,13 @@ class SendPacketActionTest {
 				didInterrupt.set(true);
 			}
 		});
+		try {
+			// This line doesn't seem to be necessary when running locally, but when running on GitHub actions,
+			//   we want to make sure that the action actually began executing.
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 		assertFalse(didInterrupt.get());
 		int numberCancelled = executorService.shutdownNow().size();
 		assertEquals(0, numberCancelled); // since it got interrupted, it actually ended so no services were cancelled
