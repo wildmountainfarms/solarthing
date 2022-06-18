9c9f09dd429f5b7baec841f765cc6a4a31b43119
Joshua Shannon <retrodaredevil@gmail.com>
2022-06-18 00:03:04 -0500

    Updated dependencies

--- graphql/src/main/java/me/retrodaredevil/solarthing/rest/graphql/service/web/DatabaseAuthorization.java
+++ graphql/src/main/java/me/retrodaredevil/solarthing/rest/graphql/service/web/DatabaseAuthorization.java
@@ -1,7 +1,6 @@
 package me.retrodaredevil.solarthing.rest.graphql.service.web;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import me.retrodaredevil.solarthing.annotations.GraphQLInclude;
 import me.retrodaredevil.solarthing.annotations.JsonExplicit;
