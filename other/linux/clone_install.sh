11121cf206f25495366c74c67afcb6dd93660fe1
Joshua Shannon <retrodaredevil@gmail.com>
2022-11-14 23:01:57 -0600

    Default version to 2022.4.1, fixed apt-get install -y git

--- other/linux/clone_install.sh
+++ other/linux/clone_install.sh
@@ -8,7 +8,7 @@ if ! type git 1>/dev/null 2>/dev/null; then
   fi
   echo "Git is not installed, so we will try to install it now"
   apt-get update || exit 1
-  apt-get install git || exit 1
+  apt-get install -y git || exit 1
 fi
 git clone --depth=1 --single-branch https://github.com/wildmountainfarms/solarthing.git || exit 1
 
--- program/.scripts/export_version.sh
+++ program/.scripts/export_version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-export SOLARTHING_VERSION="2022.3.1"
+export SOLARTHING_VERSION="2022.4.1"
 
 if [ -n "${SOLARTHING_VERSION_OVERRIDE+x}" ]; then
   echo Using override
