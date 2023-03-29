4a15a643c6ad110586de9d93a0d647117953b425
Lavender Shannon <retrodaredevil@gmail.com>
2023-03-29 16:29:00 -0500

    Hopefully fixed update_perms so clone_install works as expected

--- other/linux/update_perms.sh
+++ other/linux/update_perms.sh
@@ -2,6 +2,11 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR/../.." || exit 1
 
+if [ "$USER" != "root" ]; then
+  echo "You must be root to update permissions!"
+  exit 1
+fi
+
 echo This updates the permission of all the files in the solarthing repository. Modifications will be made to the directory printed below:
 pwd || exit 1
 if [ "$#" -gt 0 ]; then
@@ -21,9 +26,12 @@ echo Updating...
 chmod -R g+rw . || exit 1 # all files and directories get group read/write permissions
 find . -type d -exec chmod g+xs {} \; || exit 1 # When you create a file in any directory, its group should be that of its directory
 chown -R solarthing:solarthing . || exit 1
-# Run git config after so that any permission errors can be fixed before altering this property
+git config --global --replace-all safe.directory /opt/solarthing || exit 1
+# Run git config after so that there is no chance of permission errors
 git config --local core.sharedRepository group || exit 1
 echo Done
+echo "Permissions for $(pwd) have been updated. Please run the following command without sudo. This will allow your user to access the repository easily."
+echo "  git config --global --add safe.directory /opt/solarthing"
 
 # If you screw something up while messing with permissions, this may help:
 # diff <(find . -perm /g+x -type f) <(find . -perm /o+x -type f) | grep \< | tr -d '< ' | sudo xargs chmod g-x
